0901e9381c54e615247eb753bac476a911c9ae5d
hanjt208 <h2476@hxtouzi.com>
2017-12-09 20:05:13 +0800

    Signed-off-by: hanjt208 <h2476@hxtouzi.com>原始

---
+++ docker/dockerTrader/gateway/huobiGateway/__init__.py
@@ -0,0 +1,10 @@
+# encoding: UTF-8
+
+import vtConstant
+from .huobiGateway import HuobiGateway as gateway
+
+gatewayName = 'HUOBI'
+gatewayDisplayName = '火币'
+gatewayType = vtConstant.GATEWAYTYPE_BTC
+gatewayQryEnabled = True
+
